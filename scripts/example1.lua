95ee7244b0013fad576d0a10765afe1a49810164
uniondeliver <zackariadeliver@gmail.com>
2026-02-14 18:39:39 +0100

    clean template

--- scripts/example1.lua
+++ scripts/example1.lua
@@ -1,4 +1,1 @@
--- Example Game 1 script
--- Replace this with your actual script
 
-print("[tcheks hub] Example Game 1 loaded!")
--- scripts/example2.lua
+++ scripts/example2.lua
@@ -1,4 +1,1 @@
--- Example Game 2 script
--- Replace this with your actual script
 
-print("[tcheks hub] Example Game 2 loaded!")
